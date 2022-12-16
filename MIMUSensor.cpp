26fae283a4c89f2447e8cd89944bb8c393824b1b
Travis West <travis@traviswest.ca>
2022-12-16 10:00:38 -0500

    wip on celtera/avendish inspired sensor driver

---
+++ MIMUSensor.cpp
@@ -0,0 +1,167 @@
+#include "MIMUSensor.h"
+#include <iomanip> // setw, setfill
+#include <iostream>
+
+#define READ_BYTE(addr, rx) if (!spi || !spi->read_bytes(addr, &rx, 1, spi->context)) return set_error_condition();
+#define WRITE_BYTE(addr, rx) if (!spi || !spi->write_bytes(addr, &tx, 1, spi->context)) return set_error_condition();
+
+void MIMUSensor::setup()
+{
+    // TODO: search for other sensor types on available busses
+    constexpr uint8_t whoami_addr = 0x00;
+    constexpr uint8_t whoami_expected = 0xea;
+    uint8_t whoami;
+    READ_BYTE(whoami_addr, whoami);
+    if (whoami != whoami_expected)
+    {
+        errstream << __FILE__":" << __LINE__ << ":" << __func__ << ": ICM20948 whoami reports 0x" << std::hex << int(whoami) << " instead of expected 0x" << int(whoami_expected) << "\n";
+        return set_error_condition();
+    }
+    else
+        std::cout << "icm20948 whoami reports 0x" << std::hex << int(whoami) << std::endl;
+
+    // wake up the device
+    constexpr uint8_t pwr_mgmt_1_addr = 0x06;
+    constexpr uint8_t pwr_mgmt_1_value = 0x01;
+    WRITE_BYTE(pwr_mgmt_1_addr, pwr_mgmt_1_value);
+
+    vTaskDelay(pdMS_TO_TICKS(500));
+    // configure the gyro and accelerometer
+
+    constexpr uint8_t reg_bank_sel_addr = 0x7f;
+    constexpr uint8_t user_bank_2 = 0x20;
+    WRITE_BYTE(reg_bank_sel_addr, user_bank_2);
+
+    constexpr uint8_t gyro_config_1_addr = 0x01;
+    constexpr uint8_t gyro_config_1_value = 0b00000110; // 2000 dps, no digital low pass
+    WRITE_BYTE(gyro_config_1_addr, gyro_config_1_value);
+
+    // does the averaging do anything when the dlpf is disabled?
+    // constexpr uint8_t gyro_config_2_addr = 0x02;
+    // constexpr uint8_t gyro_config_2_value = 0x01; // 2 x averaging
+    // WRITE_BYTE(gyro_config_2_addr, gyro_config_2_value);
+
+    constexpr uint8_t accel_config_1_addr = 0x14;
+    constexpr uint8_t accel_config_1_value = 0b00000100; // 8 G, no digital low pass
+    WRITE_BYTE(accel_config_1_addr, accel_config_1_value);
+
+    // does the decimator do anything when the dlpf is disabled?
+    // constexpr uint8_t accel_config_2_addr = 0x15;
+    // constexpr uint8_t accel_config_2_value = 0x00; // no decimation
+    // WRITE_BYTE(accel_config_2_addr, accel_config_2_value);
+
+    // // use the auxiliary i2c main node to set up the magnetometer
+
+    // constexpr uint8_t user_bank_3 = 0x30;
+    // WRITE_BYTE(reg_bank_sel_addr, user_bank_3);
+
+    // we use slv_4 for single byte writing when setting up, and otherwise use slv_0 for reading
+    // constexpr uint8_t i2c_slv_0_addr_addr = 0x03;
+    // constexpr uint8_t i2c_slv_4_addr_addr = 0x13;
+    // constexpr uint8_t ak09916_i2c_addr = 0x0c;
+    // WRITE_BYTE(i2c_slv_0_addr_addr, ak09916_i2c_addr);
+    // WRITE_BYTE(i2c_slv_4_addr_addr, ak09916_i2c_addr);
+
+    // constexpr uint8_t i2c_slv_4_reg_addr = 0x14;
+    // constexpr uint8_t ak09916_cntl2_addr = 0x31;
+    // WRITE_BYTE(i2c_slv_4_reg_addr, i2c_slv_4_reg_value);
+
+    // constexpr uint8_t i2c_slv_4_do_addr = 0x16;
+    // constexpr uint8_t ak09916_cntl2_value = 0x08;
+    // WRITE_BYTE(i2c_slv_4_do_addr, i2c_slv_4_do_value);
+
+    // constexpr uint8_t i2c_slv_4_ctrl_addr = 0x15;
+    // constexpr uint8_t i2c_slv_4_ctrl_value = 0xa0;
+    // WRITE_BYTE(i2c_slv_4_ctrl_addr, i2c_slv_4_ctrl_value);
+
+    // constexpr uint8_t i2c_slv_0_reg_addr = 0x04;
+    // constexpr uint8_t ak09916_data_addr = 0x17;
+    // WRITE_BYTE(i2c_slv_0_reg_addr, ak09916_data_addr);
+
+    // constexpr uint8_t i2c_slv_0_ctrl_addr = 0x05;
+    // constexpr uint8_t i2c_slv_0_ctrl_value = 0x81;
+    // WRITE_BYTE(i2c_slv_0_ctrl_addr, i2c_slv_0_ctrl_value);
+
+    constexpr uint8_t user_bank_0 = 0x00;
+    WRITE_BYTE(reg_bank_sel_addr, user_bank_0);
+
+    // TODO: set up interrupt service routine, task, etc.
+
+    // // kick off loop by enabling interrupt
+
+    // constexpr uint8_t int_pin_cfg_addr = 0x0f;
+    // constexpr uint8_t int_pin_cfg_value = 0x30;
+    // WRITE_BYTE(int_pin_cfg_addr, int_pin_cfg_value);
+
+    // constexpr uint8_t int_enable_1_addr = 0x11;
+    // constexpr uint8_t int_enable_1_value = 0x01;
+    // WRITE_BYTE(int_enable_1_addr, int_enable_1_value);
+}
+
+void MIMUSensor::loop()
+{
+    DMA_ATTR static uint8_t raw_readout[32] = {0};
+    int16_t raw_gyro[3] = {0};
+    int16_t raw_accl[3] = {0};
+    float gyro[3] = {0.0f};
+    float accl[3] = {0.0f};
+
+    if ((esp_err_t)icm_spi_read_bytes(0x2D, raw_readout, 18, (void*)this) != ESP_OK) return;
+
+    raw_accl[0] = (raw_readout[ 0] << 8) | (raw_readout[ 1] & 0xFF);
+    raw_accl[1] = (raw_readout[ 2] << 8) | (raw_readout[ 3] & 0xFF);
+    raw_accl[2] = (raw_readout[ 4] << 8) | (raw_readout[ 5] & 0xFF);
+    raw_gyro[0] = (raw_readout[ 6] << 8) | (raw_readout[ 7] & 0xFF);
+    raw_gyro[1] = (raw_readout[ 8] << 8) | (raw_readout[ 9] & 0xFF);
+    raw_gyro[2] = (raw_readout[10] << 8) | (raw_readout[11] & 0xFF);
+
+    std::cout
+            << "raw accl: " 
+                 << std::dec << std::setfill(' ') << std::setw(8) << raw_accl[0] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << raw_accl[1] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << raw_accl[2] << " "
+            << "raw gyro: "
+                 << std::dec << std::setfill(' ') << std::setw(8) << raw_gyro[0] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << raw_gyro[1] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << raw_gyro[2] << " "
+            << std::endl;
+
+    accl[0] = raw_accl[0] / 4096.0;
+    accl[1] = raw_accl[1] / 4096.0;
+    accl[2] = raw_accl[2] / 4096.0;
+    gyro[0] = raw_gyro[0] / 16.4;
+    gyro[1] = raw_gyro[1] / 16.4;
+    gyro[2] = raw_gyro[2] / 16.4;
+
+    std::cout
+            << "accl: " 
+                 << std::dec << std::setfill(' ') << std::setw(8) << accl[0] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << accl[1] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << accl[2] << " "
+            << "gyro: "
+                 << std::dec << std::setfill(' ') << std::setw(8) << gyro[0] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << gyro[1] << " "
+                 << std::dec << std::setfill(' ') << std::setw(8) << gyro[2] << " "
+            << std::endl;
+}
+
+// TODO: the rest
+void MIMUSensor::update_sleep()
+{
+}
+
+void MIMUSensor::update_accelerometer_range()
+{
+}
+
+void MIMUSensor::update_gyroscope_range()
+{
+}
+
+void MIMUSensor::update_magnetometer_range()
+{
+}
+
+void MIMUSensor::update_sampling_rate()
+{
+}
---
+++ MIMUSensor.h
@@ -0,0 +1,199 @@
+#ifndef MIMUSensor_h_INCLUDED
+#define MIMUSensor_h_INCLUDED
+
+struct MIMUSensor
+{
+    static auto name() { return "MIMUSensor"; }
+    static auto uuid() { return "8b2f5018-4760-495e-aa18-2cea7ade21a4"; }
+
+    struct inputs_t {
+        struct {
+            static auto name() { return "sleep"; }
+            void update(MIMUSensor& mimu) { mimu.update_sleep(); }
+            bool value;
+        } sleep;
+
+        struct {
+            static auto name() { return "accelerometer range"; }
+            static auto c_name() { return "accelerometer_range"; }
+            static auto unit() { return "g"; }
+            void update(MIMUSensor& mimu) { mimu.update_accelerometer_range(); }
+            float value;
+        } accelerometer_range;
+
+        struct {
+            static auto name() { return "gyroscope range"; }
+            static auto c_name() { return "gyroscope_range"; }
+            static auto unit() { return "deg/s"; }
+            void update(MIMUSensor& mimu) { mimu.update_gyroscope_range(); }
+            float value;
+        } gyroscope_range;
+
+        struct {
+            static auto name() { return "magnetometer range"; }
+            static auto c_name() { return "magnetometer_range"; }
+            static auto unit() { return "uT"; }
+            void update(MIMUSensor& mimu) { mimu.update_magnetometer_range(); }
+            float value;
+        } magnetometer_range;
+
+        struct {
+            static auto name() { return "sampling rate"; }
+            static auto c_name() { return "sampling_rate"; }
+            void update(MIMUSensor& mimu) { mimu.update_sampling_rate(); }
+            float value;
+        } sampling_rate;
+    } inputs;
+
+    enum DeviceType {ICM20948}; // TODO: add support for LSM9DS1 and maybe others
+
+    struct outputs_t {
+        struct {
+            static auto name() { return "error condition"; }
+            static auto c_name() { return "error_condition"; }
+            bool value;
+        } error_condition;
+        
+        struct {
+            static auto name() { return "device type"; }
+            static auto name() { return "device_type"; }
+            struct range {
+                std::string_view values[1] = {"ICM20948"}; 
+                DeviceType init{ICM20948};
+            };
+            DeviceType value;
+        } device_type;
+
+        struct {
+            static auto name() { return "raw"; }
+            struct {
+                static auto name() { return "accelerometer"; }
+                struct {
+                    static auto name() { return "sensitivity"; }
+                    static auto unit() { return "m/s/s/LSB"; }
+                    float value;
+                } sensitivity;
+                struct {
+                    static auto name() { return "output data rate"; }
+                    static auto c_name() { return "odr"; }
+                    float value;
+                } odr;
+                uint16_t x;
+                uint16_t y;
+                uint16_t z;
+            } accelerometer;
+            struct {
+                static auto name() { return "gyroscope"; }
+                struct {
+                    static auto name() { return "sensitivity"; }
+                    static auto unit() { return "deg/s/LSB"; }
+                    float value;
+                } sensitivity;
+                struct {
+                    static auto name() { return "output data rate"; }
+                    static auto c_name() { return "odr"; }
+                    float value;
+                } odr;
+                uint16_t x;
+                uint16_t y;
+                uint16_t z;
+            } gyroscope;
+            struct {
+                static auto name() { return "magnetometer"; }
+                struct {
+                    static auto name() { return "sensitivity"; }
+                    static auto unit() { return "uT/LSB"; }
+                    float value;
+                } sensitivity;
+                struct {
+                    static auto name() { return "output data rate"; }
+                    static auto c_name() { return "odr"; }
+                    float value;
+                } odr;
+                uint16_t x;
+                uint16_t y;
+                uint16_t z;
+            } magnetometer;
+        } raw;
+
+        struct {
+            static auto name() { return "normalized"; }
+            struct {
+                static auto name() { return "accelerometer"; }
+                static auto unit() { return "m/s/s"; }
+                struct {
+                    static auto name() { return "range"; }
+                    struct {
+                        static auto name() { return "min"; }
+                        float value;
+                    } min;
+                    struct {
+                        static auto name() { return "max"; }
+                        float value;
+                    } max;
+                } range;
+                float x;
+                float y;
+                float z;
+            } accelerometer;
+            struct {
+                static auto name() { return "gyroscope"; }
+                static auto unit() { return "deg/s"; }
+                struct {
+                    static auto name() { return "range"; }
+                    struct {
+                        static auto name() { return "min"; }
+                        float value;
+                    } min;
+                    struct {
+                        static auto name() { return "max"; }
+                        float value;
+                    } max;
+                } range;
+                float x;
+                float y;
+                float z;
+            } gyroscope;
+            struct {
+                static auto name() { return "magnetometer"; }
+                static auto unit() { return "uT"; }
+                struct {
+                    static auto name() { return "range"; }
+                    struct {
+                        static auto name() { return "min"; }
+                        float value;
+                    } min;
+                    struct {
+                        static auto name() { return "max"; }
+                        float value;
+                    } max;
+                } range;
+                float x;
+                float y;
+                float z;
+            } magnetometer;
+        } normalized;
+    } outputs;
+
+    struct spi_t {
+        bool (*read_bytes)  (uint8_t reg,       uint8_t * buffer, uint8_t length, void * context);
+        bool (*write_bytes) (uint8_t reg, const uint8_t * buffer, uint8_t length, void * context);
+        void * context;
+    };
+    spi_t * spi;
+    // TODO: add support for I2C
+
+    void setup();
+
+    void loop();
+
+private:
+    void update_sleep();
+    void update_accelerometer_range();
+    void update_gyroscope_range();
+    void update_magnetometer_range();
+    void update_sampling_rate();
+    void set_error_condition() { outputs.error_condition.value = true; }
+};
+
+#endif // MIMUSensor_h_INCLUDED
